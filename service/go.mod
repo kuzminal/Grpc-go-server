82273906ee441f8c73b3d14b8c39ba9ef82ef918
kuzminal <kuzmin.al35@gmail.com>
2021-10-26 15:54:46 +0300

    Сощдал для опытов сервер grpc

---
+++ service/go.mod
@@ -0,0 +1,17 @@
+module productinfo/service
+
+go 1.17
+
+require (
+	github.com/gofrs/uuid v4.1.0+incompatible
+	google.golang.org/grpc v1.41.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20211021150943-2b146023228c // indirect
+)
---
+++ service/productinfo_ service.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"github.com/gofrs/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	pb "productinfo/service/ecommerce"
+)
+
+// Сервер используется для реализации ecommerce/product_info
+type server struct {
+	productMap map[string]*pb.Product
+}
+
+// AddProduct реализует ecommerce.AddProduct
+func (s *server) AddProduct(ctx context.Context,
+	in *pb.Product) (*pb.ProductID, error) {
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal,
+			"Error while generating Product ID", err)
+	}
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+	s.productMap[in.Id] = in
+	log.Printf("Product was added " + in.Description)
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+// GetProduct реализует ecommerce.GetProduct
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+	value, exists := s.productMap[in.Value]
+	if exists {
+		return value, status.New(codes.OK, "").Err()
+	}
+	return nil, status.Errorf(codes.NotFound, "Product does not exist.", in.Value)
+}
